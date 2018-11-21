b2079f540e07a47767b10edbcdc7062c1bf7e260
Chris Bruce <chris@chrisbruce.com>
2018-11-21 14:40:56 -0800

    adds dotenv support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+
 fn main() {
-    print!("Hello from CryptoJack!")   
+    dotenv().ok();
+
+    let api_key = std::env::var("SLACK_API_TOKEN").expect("SLACK_API_TOKEN was not found.");
+
+    print!("Hello from CryptoJack! with {:?}", api_key)   
 }
