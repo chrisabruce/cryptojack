0df4cb219cdb6fd0979106baf5f7b303121387c5
Chris Bruce <chris@chrisbruce.com>
2018-11-21 16:56:36 -0800

    adds response to mentions

--- src/bot.rs
+++ src/bot.rs
@@ -1,12 +1,38 @@
-
+use regex::Regex;
 use slack::{self, Event, RtmClient};
 
-pub struct CryptoJackBot;
+pub struct CryptoJackBot {
+    pub name: String,
+}
 
+impl CryptoJackBot {
+    pub fn new(name: &String) -> CryptoJackBot {
+        CryptoJackBot {
+            name: name.clone(),
+        }
+    }
+
+    fn on_message(&self, client: &RtmClient, message: slack::Message) {
+        match message {
+            slack::Message::Standard(message) => {
+                print!("message");
+                if let Some(output) = has_bot_mention(&self, &message.text) {
+                    let channel_id = message.channel.unwrap();
+                    let _ = client.sender().send_message(&channel_id, &output);
+                };
+            }
+            _ => println!("other"),
+        }
+}
+}
 
 impl slack::EventHandler for CryptoJackBot {
     fn on_event(&mut self, client: &RtmClient, event: Event) {
         println!("on_event(event: {:?})", event);
+        match event {
+            Event::Message(reference) => self.on_message(client, *reference),
+            _ => println!("other:"),
+        }
     }
 
     fn on_close(&mut self, client: &RtmClient) {
@@ -15,5 +41,20 @@ impl slack::EventHandler for CryptoJackBot {
 
     fn on_connect(&mut self, client: &RtmClient) {
         println!("on_connect");
+    }
 }
+
+fn has_bot_mention(bot: &CryptoJackBot, message: &Option<String>) -> Option<String> {
+    match message {
+        &Some(ref text) => {
+            let re = Regex::new(r"@(?P<bot>[\w_]+)").unwrap();
+            for caps in re.captures_iter(&text) {
+                if bot.name == &caps["bot"] {
+                    return Some(String::from("Hi there!"));
+                };
+            }
+            None
+        }
+        _ => None,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate dotenv;
+extern crate regex;
 extern crate slack;
 
 use dotenv::dotenv;
@@ -10,13 +11,14 @@ fn main() {
     dotenv().ok();
 
     let api_key = std::env::var("SLACK_API_TOKEN").expect("SLACK_API_TOKEN was not found.");
+    let bot_name = std::env::var("SLACK_BOT_NAME").expect("SLACK_BOT_NAME was not found.");
 
-    let mut bot = bot::CryptoJackBot{};
+    let mut bot = bot::CryptoJackBot::new(&bot_name);
     let r = RtmClient::login_and_run(&api_key, &mut bot);
 
     println!("{:?}", r);
     match r {
         Ok(_) => {}
         Err(err) => panic!("Error: {}", err),
-} 
+    }
 }
